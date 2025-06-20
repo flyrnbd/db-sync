c9ca2e7903c062c9599c99cca48c6031c093c931
Masud Parvez Sajjad <per.sajjadahmed@gmail.com>
2025-06-20 21:04:24 +0600

    remove imoported code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 #include<filesystem>
 #include<vector>
+#include"lib/input_schema.h"
 // ANSI escape codes for colors and styles
-#define RESET       "\033[0m"
-#define BOLD        "\033[1m"
-#define UNDERLINE   "\033[4m"
-#define RED         "\033[31m"
-#define GREEN       "\033[32m"
-#define YELLOW      "\033[33m"
-#define BLUE        "\033[34m"
-#define MAGENTA     "\033[35m"
-#define CYAN        "\033[36m"
-#define WHITE       "\033[37m"
-#define ON_RED      "\033[41m"
-#define ON_GREEN    "\033[42m"
+//#define RESET       "\033[0m"
+//#define BOLD        "\033[1m"
+//#define UNDERLINE   "\033[4m"
+//#define RED         "\033[31m"
+//#define GREEN       "\033[32m"
+//#define YELLOW      "\033[33m"
+//#define BLUE        "\033[34m"
+//#define MAGENTA     "\033[35m"
+//#define CYAN        "\033[36m"
+//#define WHITE       "\033[37m"
+//#define ON_RED      "\033[41m"
+//#define ON_GREEN    "\033[42m"
 using namespace std;
 
 
@@ -28,28 +29,6 @@ int main(int argc,char* args[]){
     //std::cout << BOLD << GREEN << "This is bold green text." << RESET << std::endl;
     //std::cout << UNDERLINE << YELLOW << "Underlined yellow text." << RESET << std::endl;
     //std::cout << BLUE << WHITE << "White text on blue background." << RESET << std::endl;
-    //std::cout << "Back to default." << std::endl;
-
-  
-  const int baseParamLength = 4;
-  const int configParamLength = 6;
-
-
-  std::string parameter[baseParamLength] = {
-    "config",
-    "storage",
-    "source",
-    "output"
-  };
-
-  std::string config_parameter[configParamLength] = {
-    "database",
-    "db_host",
-    "port",
-    "database_name",
-    "username",
-    "password"
-  };
 
   int inputCount = 1;
   while(inputCount < argc-1){
