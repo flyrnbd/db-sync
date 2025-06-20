0adba019c35baebfc01315231ffd24c643a937f2
Masud Parvez Sajjad <per.sajjadahmed@gmail.com>
2025-06-20 21:05:05 +0600

    remove imoported code

--- main.cpp
+++ main.cpp
@@ -2,19 +2,6 @@
 #include<filesystem>
 #include<vector>
 #include"lib/input_schema.h"
-// ANSI escape codes for colors and styles
-//#define RESET       "\033[0m"
-//#define BOLD        "\033[1m"
-//#define UNDERLINE   "\033[4m"
-//#define RED         "\033[31m"
-//#define GREEN       "\033[32m"
-//#define YELLOW      "\033[33m"
-//#define BLUE        "\033[34m"
-//#define MAGENTA     "\033[35m"
-//#define CYAN        "\033[36m"
-//#define WHITE       "\033[37m"
-//#define ON_RED      "\033[41m"
-//#define ON_GREEN    "\033[42m"
 using namespace std;
 
 
