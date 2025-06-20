fbc9606798069e4a55d4bb2bf236f7ce7746e137
Masud Parvez Sajjad <per.sajjadahmed@gmail.com>
2025-06-20 21:03:58 +0600

    lib added

---
+++ lib/input_schema.h
@@ -0,0 +1,43 @@
+#ifndef INPUT_SCHEMA
+#define INPUT_SCHEMA
+
+// ANSI escape code
+#define RESET       "\033[0m"
+#define BOLD        "\033[1m"
+#define UNDERLINE   "\033[4m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define BLUE        "\033[34m"
+#define MAGENTA     "\033[35m"
+#define CYAN        "\033[36m"
+#define WHITE       "\033[37m"
+#define ON_RED      "\033[41m"
+#define ON_GREEN    "\033[42m"
+
+
+
+
+const int baseParamLength = 4;
+const int configParamLength = 6;
+
+
+std::string parameter[baseParamLength] = {
+    "config",
+    "storage",
+    "source",
+    "output"
+  };
+
+std::string config_parameter[configParamLength] = {
+    "database",
+    "db_host",
+    "port",
+    "database_name",
+    "username",
+    "password"
+};
+
+
+
+#endif // INPUT_SCHEMA
